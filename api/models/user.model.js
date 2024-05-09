ca4e4ba65bc921f1e2927208ddc17382ee54319e
alaminxpro <alaminxpro@gmail.com>
2024-05-09 20:57:47 +0600

    User Model

---
+++ api/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Scheme = mongoose.Scheme(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model('User', Scheme);
+export default User;
